ad5023089b1f3463484eb52676b2d55a10d3b615
raedzayoud <raed.zayoud.23@gmail.com>
2025-07-27 23:48:19 +0200

    E & F CodeForces

---
+++ recursion/F. Creating expression2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int Backtrack(int sum, vector<int> v, int start, int goal)
+{
+
+    if (start == v.size())
+    {
+        if (sum == goal)
+        {
+            return 1;
+        }
+        return 0;
+    }
+    int neg = Backtrack(sum + (-1 * v[start]), v, start + 1, goal);
+    int pos = Backtrack(sum + (v[start]), v, start + 1, goal);
+    return neg + pos;
+}
+
+int main()
+{
+    int n, z;
+    cin >> n >> z;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    cout << Backtrack(v[0], v, 1, z);
+    return 0;
+}
